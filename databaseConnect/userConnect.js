f0600209141847d546e366145a095074429b93f9
m-siegel <mea.siegel@gmail.com>
2022-10-24 15:25:58 -0700

    updates for testing practice. will delete anyway

--- databaseConnect/userConnect.js
+++ databaseConnect/userConnect.js
@@ -1,10 +1,10 @@
 const db = [
-  { name: "Bob", _id: 1 },
-  { name: "Joe", _id: 2 },
+  { name: "Bob", _id: 1, username: "a", password: "a1" },
+  { name: "Joe", _id: 2, username: "b", password: "b2" },
 ];
 
 export async function getUserByUsername(username) {
-  return db.find((user) => user.userName === username);
+  return db.find((user) => user.username === username);
 }
 
 export async function getUserById(id) {
@@ -12,6 +12,11 @@ export async function getUserById(id) {
 }
 
 export async function addUser(userObj) {
-  db.push({ name: userObj.first_name, _id: db.length + 1 });
+  db.push({
+    name: userObj.first_name,
+    _id: db.length + 1,
+    username: userObj.username,
+    password: userObj.password,
+  });
   // add to database
 }
