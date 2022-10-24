a2891ab8d7920e94d6abbc4a7598670903bfbb96
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-10-24 15:57:43 -0700

    change user_id to _id

--- util/passport-config.js
+++ util/passport-config.js
@@ -26,7 +26,7 @@ function initialize(passport, getUserByUsername, getUserById) {
     new LocalStrategy({ usernameField: "username" }, authenticateUser)
   );
   passport.serializeUser((user, done) => {
-    return done(null, user.user_id);
+    return done(null, user._id);
   });
   passport.deserializeUser((user_id, done) => {
     return done(null, getUserById(user_id));
