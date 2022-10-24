d612082d6c33ccf44c28f2b8cb25a468e9e7d28f
m-siegel <mea.siegel@gmail.com>
2022-10-24 15:27:42 -0700

    file with path to public dir

---
+++ routes/pathToPublicDir.js
@@ -0,0 +1,7 @@
+// To get absolute path based on current location
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToPublicDir = resolve(__dirname, "../public");
+
+export default pathToPublicDir;
