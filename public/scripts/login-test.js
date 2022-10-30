2ba936e305a1a79845e09e88b0c1001cc0581358
m-siegel <mea.siegel@gmail.com>
2022-10-30 14:43:25 -0700

    swithed to using util.checkAuthenticated with callbacks

--- public/scripts/login-test.js
+++ public/scripts/login-test.js
@@ -3,48 +3,78 @@ import util from "./util.js";
 function LoginTest() {
   const loginTest = {};
 
-  // TODO -- replace with util function
-  loginTest.authenticatePage = async function () {
-    let res = await (
-      await fetch("/getAuthentication", { method: "POST" })
-    ).json();
-    if (!res.authenticated) {
-      console.log("not auth");
-      loginTest.setUpPage();
-    } else {
-      // Redirect if logged in
-      console.log("auth");
-      util.redirect("/logged-in-demo");
-    }
+  loginTest.setUpPage = async function () {
+    await util.checkNotAuthenticated(
+      "/logged-in-demo",
+
+      // Calback for page setup if valid to show page
+      async function () {
+        // set up login form listener
+        const form = document.querySelector("form");
+        form.addEventListener("submit", async (evt) => {
+          evt.preventDefault();
+          let res;
+          try {
+            res = await fetch("/loginUser", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(
+                Object.fromEntries(new FormData(form).entries())
+              ),
+            });
+            if (res.ok) {
+              res = await res.json();
+              if (res.success) {
+                util.redirect("/logged-in-demo");
+              } else {
+                util.showNeutralMessage(res.msg);
+              }
+            }
+          } catch (err) {
+            util.showErrorMessage(err);
+          }
+        });
+      },
+
+      null
+    );
   };
 
-  loginTest.setUpPage = function () {
-    // set up login form listener
-    const form = document.querySelector("form");
-    form.addEventListener("submit", async (evt) => {
-      evt.preventDefault();
-      let res;
-      try {
-        res = await fetch("/loginUser", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(
-            Object.fromEntries(new FormData(form).entries())
-          ),
-        });
-        if (res.ok) {
-          res = await res.json();
-          if (res.success) {
-            util.redirect("/logged-in-demo");
-          } else {
-            util.showNeutralMessage(res.msg);
-          }
-        }
-      } catch (err) {
-        util.showErrorMessage(err);
-      }
-    });
-  };
+  // loginTest.authenticatePage = async function () {
+  //   await util.checkNotAuthenticated(
+  //     "/logged-in-demo",
+  //     loginTest.setUpPage,
+  //     null
+  //   );
+  // };
+
+  // loginTest.setUpPage = function () {
+  //   // set up login form listener
+  //   const form = document.querySelector("form");
+  //   form.addEventListener("submit", async (evt) => {
+  //     evt.preventDefault();
+  //     let res;
+  //     try {
+  //       res = await fetch("/loginUser", {
+  //         method: "POST",
+  //         headers: { "Content-Type": "application/json" },
+  //         body: JSON.stringify(
+  //           Object.fromEntries(new FormData(form).entries())
+  //         ),
+  //       });
+  //       if (res.ok) {
+  //         res = await res.json();
+  //         if (res.success) {
+  //           util.redirect("/logged-in-demo");
+  //         } else {
+  //           util.showNeutralMessage(res.msg);
+  //         }
+  //       }
+  //     } catch (err) {
+  //       util.showErrorMessage(err);
+  //     }
+  //   });
+  // };
 
   return loginTest;
 }
