52cfa5a18b4447fd1b6549cae7b1f5118bd26b29
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-11-01 22:01:33 -0700

    tidying up, removing unused code, etc.

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 /*Armen Sarkisian and Ilana-Mahmea Siegel (pair programming) */
 import express from "express";
-// import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import dotenv from "dotenv";
-import flash from "express-flash";
+// import flash from "express-flash";
 import session from "express-session";
-import methodOverride from "method-override";
+// import methodOverride from "method-override";
 import passport from "passport";
-
-// import editDeckRouter from "./routes/edit-deck.js";
-// import exploreRouter from "./routes/explore.js";
-// import indexRouter from "./routes/index.js";
-// import loginRouter from "./routes/login.js";
-// import myLibraryRouter from "./routes/my-library.js";
-// import registerRouter from "./routes/register.js";
-// import studyRouter from "./routes/study.js";
-// import userSettingsRouter from "./routes/user-settings.js";
-// import logoutRouter from "./routes/logout.js";
-// import welcomePRouter from "./routes/welcomeP.js";
-// import homePRouter from "./routes/homeP.js";
-
+import mainRouter from "./routes/routes.js";
 import initializePassport from "./util/passport-config.js";
 import {
   getUserByUsername,
   getUserById,
 } from "./databaseConnect/userConnect.js";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToPublicDir = resolve(__dirname, "./public");
 
 dotenv.config();
 
@@ -35,7 +26,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // https://www.youtube.com/watch?v=-RCnNyD0L-s
-app.use(flash());
+// app.use(flash());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -46,24 +37,10 @@ app.use(
 initializePassport(passport, getUserByUsername, getUserById);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
 app.use(cookieParser());
 // This is needed to serve html pages with styling
-// TODO -- figure out how to deal with .html
-app.use(express.static("./public"));
-
-// app.use("/edit-deck", editDeckRouter);
-// app.use("/explore", exploreRouter);
-// // app.use("/", indexRouter);
-// app.use("/login", loginRouter);
-// app.use("/my-library", myLibraryRouter);
-// app.use("/register", registerRouter);
-// app.use("/study", studyRouter);
-// app.use("/user-settings", userSettingsRouter);
-// app.use("/logout", logoutRouter);
-
-// TODO -- better formatting
-import mainRouter from "./routes/routes.js";
+app.use(express.static(pathToPublicDir));
 app.use("/", mainRouter);
 
 export default app;
--- routes/oldBackEnd/pathToPublicDir.js
+++
@@ -1,8 +0,0 @@
-/*Ilana-Mahmea Siegel*/
-// To get absolute path based on current location
-import { dirname, resolve } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const pathToPublicDir = resolve(__dirname, "../public");
-
-export default pathToPublicDir;
